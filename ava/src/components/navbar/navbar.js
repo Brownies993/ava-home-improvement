00e2905dc7ed2712bdb2b3b5228571d187fe33c1
Anthony Hernandez <abher216@hotmail.com>
2018-07-07 11:12:23 -0400

    changes

---
+++ ava/src/components/navbar/navbar.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const Navbar = props => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <NavLink className="navbar-brand" to="/" activeClassName="active">AVA Home Improvement LLC</NavLink>
+      <div className="collapse navbar-collapse" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item active">
+            <NavLink className="nav-link" to="/about" activeClassName="active">About </NavLink>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar;
